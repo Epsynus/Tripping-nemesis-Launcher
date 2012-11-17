880eee8742a62317ff4f2f29a685f6a3bf576b32
Jesper Karlsson <epsynus@live.se>
2012-11-17 18:44:36 +0100

    Renaming & added functionality
    Renamed a lot of the control's for easier readability also added
    comment's as well as adding functionality to ceirtan buttons.

--- Tripping-Nemesis Launcher/Tripping-Nemesis Launcher/Form1.cs
+++ Tripping-Nemesis Launcher/Tripping-Nemesis Launcher/Form1.cs
@@ -19,6 +19,7 @@ namespace Tripping_Nemesis_Launcher
 
         private void SaveCheckBox_CheckedChanged(object sender, EventArgs e)
         {
+            //If the save checkbox is checked it will enable the button to select where to save, else it will keep the button disabled
             if (SaveCheckBox.Checked == true)
                 SaveLogButton.Enabled = true;
             else
@@ -27,6 +28,7 @@ namespace Tripping_Nemesis_Launcher
 
         private void RestartDeadServerCheckBox_CheckedChanged(object sender, EventArgs e)
         {
+            //If the Restart dead server check box is checked it will allow you to choose how long it will take before it will automatically restart incase it crashes
             if (RestartDeadServerCheckBox.Checked == false)
                 RestartDeadServerNumeric.Enabled = false;
             else
@@ -35,10 +37,62 @@ namespace Tripping_Nemesis_Launcher
 
         private void MonitorCPUUsageCheckBox_CheckedChanged(object sender, EventArgs e)
         {
+            //If the Monitor CPU Usage checkbox is checked it will allow you to choose the interval at which it will check and report the CPU usage that the server has at that current time.
             if (MonitorCPUUsageCheckBox.Checked == false)
                 MonitorCPUUsageNumeric.Enabled = false;
             else
                 MonitorCPUUsageNumeric.Enabled = true;
+        }
+
+        private void SaveLogButton_Click(object sender, EventArgs e)
+        {
+            //saves the server logfile as a plain text document with the .log extension, if they press cancel or saves the file it unticks the checkbox.
+            // as well as clears the log window
+            if (saveLogFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ServerHistoryLog.SaveFile(saveLogFileDialog.FileName, RichTextBoxStreamType.PlainText);
+                    SaveCheckBox.Checked = false;
+                    ServerHistoryLog.Text = "";
+            }
+            else
+            {
+                SaveCheckBox.Checked = false;
+            }
+        }
+        private void selectLoginServerExeButton_Click(object sender, EventArgs e)
+        {
+            if (openLoginServerexeDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Do stuff
+                StartLoginServerExeButton.Enabled = true;
+                SetLoginServerPriority.Enabled = true;
+                LoginLabel.Text = "Server Ready.";
+                LoginGroupBox.Text = "Login Server: " + openLoginServerexeDialog.SafeFileName;
+            }
+        }
+
+        private void selectCharServerExeButton_Click(object sender, EventArgs e)
+        {
+            if (openCharServerexeDialog.ShowDialog() == DialogResult.OK)
+            {
+                //do stuff
+                StartCharServerExeButton.Enabled = true;
+                SetCharServerPriority.Enabled = true;
+                CharLabel.Text = "Server Ready.";
+                CharacterGroupBox.Text = "Character Server: " + openCharServerexeDialog.SafeFileName;
+            }
+        }
+
+        private void selectMapServerExeButton_Click(object sender, EventArgs e)
+        {
+            if (openMapServerexeDialog.ShowDialog() == DialogResult.OK)
+            {
+                //do stuff
+                StartMapServerExeButton.Enabled = true;
+                SetMapServerPriority.Enabled = true;
+                MapLabel.Text = "Server Ready.";
+                MapGroupBox.Text = "Map Server: " + openMapServerexeDialog.SafeFileName;
+            }
         }
     }
 }
